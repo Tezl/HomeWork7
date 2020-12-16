018384b4de3195f2f4b7d14a4e9af211f043e74c
Fistashka <tizilpe@gmail.com>
2020-12-16 21:57:10 +0200

    Initial commit

---
+++ src/HomeWork7.java
@@ -0,0 +1,30 @@
+public class HomeWork7 {
+
+    public static class Util {
+
+
+        public static double getDistance(int x1, int y1, int x2, int y2) {
+
+            x1 = 7;
+            x2 = 3;
+            y1 = 8;
+            y2 = 2;
+
+            int a = x2 - x1;
+            int b = a * a; // a^2
+            int c = y2 - y1;
+            int d = c * c; // c ^2
+
+            int e = (b) + (d); // это - (x2-x1)^2 + (y2-y1)^2
+
+            Math.sqrt(e); //извлекает из выше перечисленного корень
+
+// выше - формула √(x2-x1)^2 + (y2-y1)^2
+
+            System.out.println("расстояние между точками = " + e);
+
+            return Math.sqrt(e);
+        }
+
+    }
+}
